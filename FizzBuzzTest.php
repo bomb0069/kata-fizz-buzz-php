82bed751f86c0fd9d2d928c3e6e8ab6ba97964bb
armeo <arm@prontomarketing.com>
2014-03-20 00:02:12 +0700

    add test fail when number=6

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -3,29 +3,34 @@ require_once ('FizzBuzz.php');
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase {
 
-  function setUp(){
-    $this->fizzBuzz = new FizzBuzz();
-  }
+    function setUp(){
+        $this->fizzBuzz = new FizzBuzz();
+    }
 
-  function testFizzBuzzWith1ShouldBeReturn1() {
-    $actual = $this->fizzBuzz->getString(1);
-		$this->assertEquals('1', $actual);
-	}
+    function testFizzBuzzWith1ShouldBeReturn1() {
+        $actual = $this->fizzBuzz->getString(1);
+        $this->assertEquals('1', $actual);
+    }
 
-  function testFizzBuzzWith2ShouldBeReturn2() {
-    $actual = $this->fizzBuzz->getString(2);
-		$this->assertEquals('2', $actual);
-  }
+    function testFizzBuzzWith2ShouldBeReturn2() {
+        $actual = $this->fizzBuzz->getString(2);
+        $this->assertEquals('2', $actual);
+    }
 
-  function testFizzBuzzWith3ShouldBeReturnFizz(){
-    $actual = $this->fizzBuzz->getString(3);
-    $this->assertEquals('Fizz',$actual);
-  }
+    function testFizzBuzzWith3ShouldBeReturnFizz(){
+        $actual = $this->fizzBuzz->getString(3);
+        $this->assertEquals('Fizz',$actual);
+    }
 
-  function testFizzBuzzWith5ShouldBeReturnBuzz(){
-    $actual = $this->fizzBuzz->getString(5);
-    $this->assertEquals('Buzz',$actual);
-  }
+    function testFizzBuzzWith5ShouldBeReturnBuzz(){
+        $actual = $this->fizzBuzz->getString(5);
+        $this->assertEquals('Buzz',$actual);
+    }
+
+    function testFizzBuzzWith6ShouldBeReturnFizz(){
+        $actual = $this->fizzBuzz->getString(6);
+        $this->assertEquals('Fizz',$actual);
+    }
 }
 
 ?>
