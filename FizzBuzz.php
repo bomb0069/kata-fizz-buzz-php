04229ffd72835e4d88d01ffe28ab5e590d24542d
armeo <arm@prontomarketing.com>
2014-03-19 23:59:24 +0700

    solve number=5

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,6 +5,8 @@ class FizzBuzz {
     function getString($input) {
     	if ($input == 3)
     		return 'Fizz';
+        elseif ($input == 5)
+            return 'Buzz';
     	return $input;
     }
 }
