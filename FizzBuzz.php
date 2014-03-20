5bc49345eb38ebd9c847add50915092228092fef
BumBimz <l3uml3imz@gmail.com>
2014-03-20 07:01:17 +0700

    solve number is 6

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -3,7 +3,7 @@
 class FizzBuzz {
 
     function getString($input) {
-    	if ($input == 3)
+    	if ($input % 3 == 0)
     		return 'Fizz';
         elseif ($input == 5)
             return 'Buzz';
