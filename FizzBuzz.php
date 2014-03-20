a05b2fb6a4ac82b3296a68bd4a4709a0bf4b4158
karan <bomb0069@gmail.com>
2014-03-20 20:46:58 +0700

    solve testFizzBuzzWith10ShouldBeReturnBuzz

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,7 +5,7 @@ class FizzBuzz {
     function getString($input) {
     	if ($input % 3 == 0)
     		return 'Fizz';
-        elseif ($input == 5)
+        elseif ($input % 5 == 0)
             return 'Buzz';
     	return $input;
     }
